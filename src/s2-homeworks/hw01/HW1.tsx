4beab23716fd18b33767a6e724c23616a473d7a0
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-01-16 21:40:09 +0400

    hw_Monday_week_1_done

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Message from './message/Message'
+import Message, {MessagePropsType} from './message/Message'
 import MessageSender from './message-sender/MessageSender'
 import s2 from '../../s1-main/App.module.css'
 import FriendMessage from './friend-message/FriendMessage'
-import avatar from './avatar.png'
+import groot from './groot.png'
 
 /*
 * 1 - описать тип MessageType
@@ -14,29 +14,39 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+    id: number
+    user: {
+        avatar: string
+        name: string
+    },
+    message: {
+        text: string
+        time: string
+    }
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
     id: 0,
     user: {
-        avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        avatar: groot, // можно менять
+        name: 'Groot',  // можно менять
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
+        text: 'i am groot', // можно менять
         time: '22:00', // можно менять
     },
 }
 export const friendMessage0: MessageType = {
     id: 100,
     user: {
-        avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
+        avatar: groot, // можно менять
+        name: 'Groot', // можно менять
     },
     message: {
-        text: 'зеркальное сообщение для тренировки css', // можно менять
-        time: '22:00', // можно менять
+        text: 'i am groot', // можно менять
+        time: '22:05', // можно менять
     },
 }
 
