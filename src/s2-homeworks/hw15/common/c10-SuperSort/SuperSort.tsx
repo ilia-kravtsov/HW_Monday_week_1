60a52422d5d18c62807a2949133aeff55647fc78
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-05-26 22:39:19 +0400

    1

--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -17,6 +17,60 @@ export type SuperSortPropsType = {
 }
 
 export const pureChange = (sort: string, down: string, up: string) => {
+    return sort === down ? up : sort == up ? '' : down
+}
+
+const SuperSort: React.FC<SuperSortPropsType> = (
+    {
+        sort, value, onChange, id = 'hw15',
+    }
+) => {
+    const up = '0' + value
+    const down = '1' + value
+
+    const onChangeCallback = () => {
+        onChange(pureChange(sort, down, up))
+    }
+
+    const icon = sort === down ? downIcon : sort === up ? upIcon : noneIcon
+
+    return (
+        <span
+            id={id + '-sort-' + value}
+            onClick={onChangeCallback}
+        >
+            <img
+                id={id + '-icon-' + sort}
+                src={icon}
+                className={s.iconBord}
+            />
+        </span>
+    )
+}
+
+export default SuperSort
+
+
+/*
+import React from 'react'
+import upicon from './up.png'
+import downicon from './down.png'
+import upAndDown from './upanddown.png'
+import s from '../../HW15.module.css'
+
+// добавить в проект иконки и импортировать
+const downIcon = downicon
+const upIcon = upicon
+const noneIcon = upAndDown
+
+export type SuperSortPropsType = {
+    id?: string
+    sort: string
+    value: string
+    onChange: (newSort: string) => void
+}
+
+export const pureChange = (sort: string, down: string, up: string) => {
     return sort === '' ? down : sort === down ? up : sort === up ? '' : down
 }
 
@@ -53,3 +107,4 @@ const SuperSort: React.FC<SuperSortPropsType> = (
 }
 
 export default SuperSort
+ */
